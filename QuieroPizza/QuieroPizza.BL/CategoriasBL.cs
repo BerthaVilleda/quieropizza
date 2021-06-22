dbd0482c0bc4f74eb771548d0ba9fe2ec0de9316
Bertha Villeda <fbertha121@gmail.com>
2021-06-22 13:16:52 -0600

    Actualizado

---
+++ QuieroPizza/QuieroPizza.BL/CategoriasBL.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuieroPizza.BL
+{
+    public class CategoriasBL
+    {
+        Contexto _contexto;
+        public List<Categoria> ListadeCategorias { get; set; }
+
+        public CategoriasBL()
+        {
+            _contexto = new BL.Contexto();
+            ListadeCategorias = new List<Categoria>();
+        }
+
+        public List<Categoria> ObtenerCategorias()
+        {
+            ListadeCategorias = _contexto.Categorias.ToList();
+            return ListadeCategorias;
+        }
+
+        public void GuardarCategoria(Categoria categoria)
+        {
+            if(categoria.Id == 0)
+            {
+                _contexto.Categorias.Add(categoria);
+            } else
+            {
+                var categoriaExitente = _contexto.Categorias.Find(categoria.Id);
+                categoriaExitente.Descripcion = categoria.Descripcion;
+            }
+
+            _contexto.SaveChanges();
+        }
+
+        public Categoria ObtenerCategoria(int id)
+        {
+            var categoria = _contexto.Categorias.Find(id);
+
+            return categoria;
+        }
+    }
+}
